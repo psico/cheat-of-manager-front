9c17d4cdbbd4f0320b5089890548f69128cd5e35
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2023-11-08 20:32:42 -0300

    [ADD] Adicionado primeiro link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,21 +41,38 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            href="https://localhost:3000/relatorioOfs"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Relatório OFs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Clique aqui para ver o relatório de OFs
           </p>
         </a>
+
+        {/*<a*/}
+        {/*  href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"*/}
+        {/*  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  <h2 className={`mb-3 text-2xl font-semibold`}>*/}
+        {/*    Docs{' '}*/}
+        {/*    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">*/}
+        {/*      -&gt;*/}
+        {/*    </span>*/}
+        {/*  </h2>*/}
+        {/*  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>*/}
+        {/*    Find in-depth information about Next.js features and API.*/}
+        {/*  </p>*/}
+        {/*</a>*/}
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
