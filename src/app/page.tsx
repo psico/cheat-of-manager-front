f6db5b7916548115b5eee4a42c23157e0d0572d3
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2023-11-10 11:37:45 -0300

    [UPT] adicionado o link novo para o backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,9 +41,8 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-            href="https://localhost:3000/relatorioOfs"
+            href="http://localhost:3001/relatorioOfs"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
             rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
