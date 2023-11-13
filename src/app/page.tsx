946e16b51910723fa3c6a538b8015e8cf1404ed0
Mariane Freitas <mariane11.freitas@gmail.com>
2023-11-13 20:53:25 -0300

    Add variavel de ambiente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-            href="http://localhost:3001/relatorioOfs"
+            href={process.env.REACT_APP_URL_BACKEND}
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             rel="noopener noreferrer"
         >
