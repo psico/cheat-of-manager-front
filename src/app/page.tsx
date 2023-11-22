92ef4d20faf2ec552f3332ac21fb5899666ba473
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2023-11-22 16:57:37 -0300

    [UPT] Começando a classificação UST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,13 +80,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Classificação UST{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Igual ao relatório de OFs mas trás a classificação das UST para cada arquivo
           </p>
         </a>
 
