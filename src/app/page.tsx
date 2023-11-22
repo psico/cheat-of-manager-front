0632e62ff26bd4481913dda6d90df832e27d99eb
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2023-11-22 16:58:27 -0300

    [UPT] Começando a classificação UST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
         {/*</a>*/}
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href={`${process.env.REACT_APP_URL_BACKEND}/relatorio-ofs-classificado-ust`}
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
